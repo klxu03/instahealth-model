55cc80498942c034f765ab8231d3335b7ca9e320
galencolin <galencolin@gmail.com>
2021-07-15 12:04:54 -0400

    fix requirements.txt

--- data/colin/filled_by_doctors/processor.py
+++ data/colin/filled_by_doctors/processor.py
@@ -1,5 +1,9 @@
 import csv
 
+types = ['FAMILY PRACTICE/PRIMARY CARE', 'CARDIOLOGY', 'PULMONARY DISEASE']
+replaces = {'PULMONARY DISEASE': 'CARDIOLOGY', 'dyspnea': 'shortness of breath'}
+typeset = {}
+
 import csv
 with open('filled_by_doctors.csv', newline='') as infile:
 	with open('processed_filled_by_doctors.csv', 'w', newline='') as outfile:
@@ -7,6 +11,14 @@ with open('filled_by_doctors.csv', newline='') as infile:
 		writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		writer.writerow(['Encounter_Description', 'CC', 'Specialty'])
 		for row in reader:
-			if (row['CC'] != '' and row['Specialty'] != ''):
-				row['CC'] = row['CC'].replace('dyspnea', 'shortness of breath')
+			if (row['Specialty'] in typeset):
+				typeset[row['Specialty']] += 1
+			else:
+				typeset[row['Specialty']] = 1
+			if (row['CC'] != '' and row['Specialty'] != '' and row['Specialty'] in types):
+				for key in replaces:
+					row['CC'] = row['CC'].replace(key, replaces[key])
+					row['Specialty'] = row['Specialty'].replace(key, replaces[key])
 				writer.writerow([row['Encounter_Description'], row['CC'], row['Specialty']])
+				
+print(typeset)
